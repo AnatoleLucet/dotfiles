d72035dec67be6343a9c9beef784c8cc77d0e362
AnatoleLucet <lucet.anatole@gmail.com>
2019-12-12 21:53:07 +0100

    removing st borders

--- st/config.h
+++ st/config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = { "Inconsolata for Powerline:pixelsize=14:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
