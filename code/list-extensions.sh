1b2916410afb6c35d6d8f0666221f3dc7449af5b
AnatoleLucet <lucet.anatole@gmail.com>
2020-07-15 12:53:03 +0200

    update vscode extensions

--- code/list-extensions.sh
+++ code/list-extensions.sh
@@ -1,13 +1,16 @@
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension christian-kohler.path-intellisense
 code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension davidlday.languagetool-linter
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension dunstontc.vscode-docker-syntax
+code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
 code --install-extension eg2.vscode-npm-script
 code --install-extension esbenp.prettier-vscode
 code --install-extension FallenMax.mithril-emmet
 code --install-extension Gajesh1.React-JS-Snippet
+code --install-extension golang.go
 code --install-extension JamesBirtles.svelte-vscode
 code --install-extension jolaleye.horizon-theme-vscode
 code --install-extension mgmcdermott.vscode-language-babel
@@ -20,6 +23,6 @@ code --install-extension PKief.material-icon-theme
 code --install-extension Prisma.prisma
 code --install-extension ritwickdey.LiveServer
 code --install-extension suming.react-proptypes-generate
+code --install-extension tombonnike.vscode-status-bar-format-toggle
 code --install-extension vscodevim.vim
-code --install-extension WallabyJs.quokka-vscode
 code --install-extension xabikos.ReactSnippets
