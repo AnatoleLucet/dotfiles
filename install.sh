81aaab27b838131ad4054630467b0ac2b91c189c
AnatoleLucet <anatole@hey.com>
2021-08-09 18:00:31 +0200

    fix docker install

--- install.sh
+++ install.sh
@@ -44,7 +44,8 @@ if lsb_release -d | grep -q "Ubuntu"; then
 	echo \
 		"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 		$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	sudo apt update
+	sudo apt install -y docker-ce docker-ce-cli containerd.io
 	sudo groupadd docker
 	sudo usermod -aG docker $USER
 else 
